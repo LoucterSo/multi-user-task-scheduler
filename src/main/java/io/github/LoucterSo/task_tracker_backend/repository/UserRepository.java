be73a726b758c64df67746ba4e0f094c47c1d780
LoucterSo <vlad_gorelin@inbox.ru>
2025-01-31 19:26:46 +0300

    findByEmail method added to UserRepository

--- src/main/java/io/github/LoucterSo/task_tracker_backend/repository/UserRepository.java
+++ src/main/java/io/github/LoucterSo/task_tracker_backend/repository/UserRepository.java
@@ -4,8 +4,10 @@ package io.github.LoucterSo.task_tracker_backend.repository;
 import io.github.LoucterSo.task_tracker_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
