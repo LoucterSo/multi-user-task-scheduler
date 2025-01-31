a12a5cb6e880a6b14c35398c25898fd76eb03757
LoucterSo <vlad_gorelin@inbox.ru>
2025-01-31 19:27:04 +0300

    existsByEmail and findByEmail added to UserService

--- src/main/java/io/github/LoucterSo/task_tracker_backend/service/UserService.java
+++ src/main/java/io/github/LoucterSo/task_tracker_backend/service/UserService.java
@@ -1,12 +1,23 @@
 package io.github.LoucterSo.task_tracker_backend.service;
 
+import io.github.LoucterSo.task_tracker_backend.entity.User;
 import io.github.LoucterSo.task_tracker_backend.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class UserService {
 
     private UserRepository userRepository;
+
+    public boolean existsByEmail(String email) {
+        return userRepository.existsByEmail(email);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
 }
