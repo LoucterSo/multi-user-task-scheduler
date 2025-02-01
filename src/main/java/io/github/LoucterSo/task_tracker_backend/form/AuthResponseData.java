09809b031daa056a15f8e433f0dc5698ffdbffcb
LoucterSo <vlad_gorelin@inbox.ru>
2025-02-01 11:37:56 +0300

    Access token added to authentication form

--- src/main/java/io/github/LoucterSo/task_tracker_backend/form/AuthResponseData.java
+++ src/main/java/io/github/LoucterSo/task_tracker_backend/form/AuthResponseData.java
@@ -1,5 +1,6 @@
 package io.github.LoucterSo.task_tracker_backend.form;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,4 +11,7 @@ import lombok.Setter;
 @Getter @Setter
 public class AuthResponseData {
     private String message;
+
+    @JsonProperty("access_token")
+    private String accessToken;
 }
