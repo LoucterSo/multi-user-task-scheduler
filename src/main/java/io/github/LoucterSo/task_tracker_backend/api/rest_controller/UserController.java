f31ad23467716b4e73628c0e0fc98cd70d46920e
LoucterSo <vlad_gorelin@inbox.ru>
2025-02-01 11:36:44 +0300

    All users deleting added for testing

--- src/main/java/io/github/LoucterSo/task_tracker_backend/api/rest_controller/UserController.java
+++ src/main/java/io/github/LoucterSo/task_tracker_backend/api/rest_controller/UserController.java
@@ -4,22 +4,34 @@ import io.github.LoucterSo.task_tracker_backend.entity.Authority;
 import io.github.LoucterSo.task_tracker_backend.entity.User;
 import io.github.LoucterSo.task_tracker_backend.form.AuthResponseData;
 import io.github.LoucterSo.task_tracker_backend.form.SignupForm;
+import io.github.LoucterSo.task_tracker_backend.service.JwtService;
 import io.github.LoucterSo.task_tracker_backend.service.UserService;
-import lombok.AllArgsConstructor;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.HashSet;
 
 @RestController
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserController {
-    private UserService userService;
-    private PasswordEncoder passwordEncoder;
+    private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final RedirectStrategy redirectStrategy;
+    private final AuthenticationProvider authenticationProvider;
 
     @PostMapping(value = "/user")
     public ResponseEntity<AuthResponseData> signup(@RequestBody SignupForm signupForm) {
@@ -49,11 +61,20 @@ public class UserController {
 
         userService.saveUser(user);
 
+        String jwtAccess = jwtService.generateAccessToken(user);
+
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(AuthResponseData.builder()
                         .message("Success")
+                        .accessToken(jwtAccess)
                         .build());
+    }
+
+    @GetMapping("/private")
+    public String privateS() {
+
+        return "private";
     }
 
 }
