12e711db27df2ba75981a2ea72d2787e01e0ae6a
LoucterSo <vlad_gorelin@inbox.ru>
2025-02-01 09:17:07 +0300

    Configuration annotation added and default login form enabled

--- src/main/java/io/github/LoucterSo/task_tracker_backend/security/SecurityChain.java
+++ src/main/java/io/github/LoucterSo/task_tracker_backend/security/SecurityChain.java
@@ -1,14 +1,16 @@
 package io.github.LoucterSo.task_tracker_backend.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityChain {
 
@@ -16,6 +18,7 @@ public class SecurityChain {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+                .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(httpSecurityCorsConfigurer ->
                         httpSecurityCorsConfigurer.configurationSource(request ->
