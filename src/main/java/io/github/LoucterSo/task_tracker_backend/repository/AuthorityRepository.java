a2426a83fdc88a0bff3c42dfa4327502c954d35c
LoucterSo <vlad_gorelin@inbox.ru>
2025-02-02 10:03:41 +0300

    Services added and refactored

--- src/main/java/io/github/LoucterSo/task_tracker_backend/repository/AuthorityRepository.java
+++ src/main/java/io/github/LoucterSo/task_tracker_backend/repository/AuthorityRepository.java
@@ -4,6 +4,9 @@ import io.github.LoucterSo.task_tracker_backend.entity.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+public interface AuthorityRepository extends JpaRepository<Authority, Integer> {
+    Optional<Authority> findByRole(Authority.Roles role);
 }
