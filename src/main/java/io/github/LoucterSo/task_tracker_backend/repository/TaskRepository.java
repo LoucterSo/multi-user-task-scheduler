fa439c7ff388a3a906f0825f75789dcb9cf57b55
LoucterSo <vlad_gorelin@inbox.ru>
2025-02-05 19:39:53 +0300

    Dockerfile changed

--- src/main/java/io/github/LoucterSo/task_tracker_backend/repository/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.LoucterSo.task_tracker_backend.repository;
-
-import io.github.LoucterSo.task_tracker_backend.entity.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
