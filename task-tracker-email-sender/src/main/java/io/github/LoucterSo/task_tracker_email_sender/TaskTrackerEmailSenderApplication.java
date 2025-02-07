a0147ed58505a147845fffb398f34cb5f63193fa
LoucterSo <vlad_gorelin@inbox.ru>
2025-02-07 07:27:02 +0300

    Getting started

---
+++ task-tracker-email-sender/src/main/java/io/github/LoucterSo/task_tracker_email_sender/TaskTrackerEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package io.github.LoucterSo.task_tracker_email_sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerEmailSenderApplication.class, args);
+	}
+
+}
