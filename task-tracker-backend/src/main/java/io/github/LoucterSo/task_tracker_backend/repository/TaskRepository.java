666a3c7d51d43dc539891bf32b6ef0fbe0eadd1d
LoucterSo <vlad_gorelin@inbox.ru>
2025-03-14 12:00:13 +0300

    Refactoring

--- task-tracker-backend/src/main/java/io/github/LoucterSo/task_tracker_backend/repository/TaskRepository.java
+++ task-tracker-backend/src/main/java/io/github/LoucterSo/task_tracker_backend/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package io.github.LoucterSo.task_tracker_backend.repository;
 
 import io.github.LoucterSo.task_tracker_backend.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 }
