6bf6b26de7895539a3343dd29d55930f479be99c
LoucterSo <vlad_gorelin@inbox.ru>
2025-03-21 15:44:16 +0300

    EmailDto added to use email sender

---
+++ task-tracker-backend/src/main/java/io/github/LoucterSo/task_tracker_backend/form/email/EmailDto.java
@@ -0,0 +1,5 @@
+package io.github.LoucterSo.task_tracker_backend.form.email;
+
+public record EmailDto(String to, String header, String text) {
+
+}
---
+++ task-tracker-email-sender/src/main/java/io/github/LoucterSo/task_tracker_email_sender/form/EmailDto.java
@@ -0,0 +1,5 @@
+package io.github.LoucterSo.task_tracker_email_sender.form;
+
+public record EmailDto(String to, String header, String text) {
+
+}
