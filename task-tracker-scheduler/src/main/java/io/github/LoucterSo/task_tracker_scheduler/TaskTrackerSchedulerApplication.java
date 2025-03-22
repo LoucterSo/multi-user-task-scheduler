3302ca852e44d03bf915a4a690462531c7920cf1
LoucterSo <vlad_gorelin@inbox.ru>
2025-03-22 07:29:39 +0300

    Scheduling enabled

--- task-tracker-scheduler/src/main/java/io/github/LoucterSo/task_tracker_scheduler/TaskTrackerSchedulerApplication.java
+++ task-tracker-scheduler/src/main/java/io/github/LoucterSo/task_tracker_scheduler/TaskTrackerSchedulerApplication.java
@@ -2,7 +2,9 @@ package io.github.LoucterSo.task_tracker_scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TaskTrackerSchedulerApplication {
 
