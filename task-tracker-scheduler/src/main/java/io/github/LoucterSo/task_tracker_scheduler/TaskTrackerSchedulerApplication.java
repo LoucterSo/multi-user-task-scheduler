a1a40fcda2affeb76534d9fbd8839a8e4ba4090b
LoucterSo <vlad_gorelin@inbox.ru>
2025-03-22 16:09:21 +0300

    Refactoring

--- task-tracker-scheduler/src/main/java/io/github/LoucterSo/task_tracker_scheduler/TaskTrackerSchedulerApplication.java
+++ task-tracker-scheduler/src/main/java/io/github/LoucterSo/task_tracker_scheduler/TaskTrackerSchedulerApplication.java
@@ -3,6 +3,8 @@ package io.github.LoucterSo.task_tracker_scheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
 
 @EnableScheduling
 @SpringBootApplication
