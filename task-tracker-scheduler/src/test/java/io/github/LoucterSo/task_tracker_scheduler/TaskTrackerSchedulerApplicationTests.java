bfe40abda4d1b1b64f5497bf8a65536a2a0c6bf7
LoucterSo <vlad_gorelin@inbox.ru>
2025-03-22 07:14:51 +0300

    Refactoring

---
+++ task-tracker-scheduler/src/test/java/io/github/LoucterSo/task_tracker_scheduler/TaskTrackerSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.LoucterSo.task_tracker_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
