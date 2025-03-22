34b8314195dcec78ce654f5c455adbe0d588b3a6
LoucterSo <vlad_gorelin@inbox.ru>
2025-03-22 16:11:04 +0300

    LoginForm and AuthResponseForm added

---
+++ task-tracker-scheduler/src/main/java/io/github/LoucterSo/task_tracker_scheduler/form/LoginForm.java
@@ -0,0 +1,9 @@
+package io.github.LoucterSo.task_tracker_scheduler.form;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+    private final String email;
+    private final String password;
+}
