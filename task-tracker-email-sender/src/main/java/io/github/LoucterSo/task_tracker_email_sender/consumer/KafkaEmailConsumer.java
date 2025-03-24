ede5108d45246c6e1860b7c78704bc44d425ed81
LoucterSo <vlad_gorelin@inbox.ru>
2025-03-24 13:12:04 +0300

    KafkaEmailConsumer can send emails

--- task-tracker-email-sender/src/main/java/io/github/LoucterSo/task_tracker_email_sender/consumer/KafkaEmailConsumer.java
+++ task-tracker-email-sender/src/main/java/io/github/LoucterSo/task_tracker_email_sender/consumer/KafkaEmailConsumer.java
@@ -1,14 +1,13 @@
 package io.github.LoucterSo.task_tracker_email_sender.consumer;
 
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.LoucterSo.task_tracker_email_sender.form.EmailDto;
 import io.github.LoucterSo.task_tracker_email_sender.service.MailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.MailException;
 import org.springframework.stereotype.Component;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 
@@ -20,14 +19,16 @@ public class KafkaEmailConsumer {
     private final MailService mailService;
     private final ObjectMapper objectMapper;
 
-    @KafkaListener(topics = "EMAIL_SENDING_TASKS", concurrency = "2")
+    @KafkaListener(topics = "EMAIL_SENDING_TASKS", concurrency = "3")
     public void sendEmail(String eDto) {
         try {
             EmailDto emailDto = objectMapper.readValue(eDto, EmailDto.class);
-            //mailService.sendEmail(emailDto.to(), emailDto.header(), emailDto.text());
+            mailService.sendEmail(emailDto.to(), emailDto.header(), emailDto.text());
             log.info("Text {} with header {} sent to {}", emailDto.text(), emailDto.header(), emailDto.to());
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (MailException e) {
+            log.error("Can't send email");
         }
     }
 }
